68093db38bb92f99cd93f2e1456c4f541038ce7d
huijun29 <71751608+huijun29@users.noreply.github.com>
2020-10-28 00:20:55 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,79 @@
+for (let I = 0; I <= 4; I++) {
+    for (let J = 0; J <= 4; J++) {
+        if (I + J == 8) {
+            led.plot(I, J)
+        }
+    }
+}
+basic.pause(200)
+for (let I = 0; I <= 4; I++) {
+    for (let J = 0; J <= 4; J++) {
+        if (I + J == 7) {
+            led.plot(I, J)
+        }
+    }
+}
+basic.pause(200)
+for (let I = 0; I <= 4; I++) {
+    for (let J = 0; J <= 4; J++) {
+        if (I + J == 6) {
+            led.plot(I, J)
+        }
+    }
+}
+basic.pause(200)
+for (let I = 0; I <= 4; I++) {
+    for (let J = 0; J <= 4; J++) {
+        if (I + J == 5) {
+            led.plot(I, J)
+        }
+    }
+}
+basic.pause(200)
+for (let I = 0; I <= 4; I++) {
+    for (let J = 0; J <= 4; J++) {
+        if (I + J == 4) {
+            led.plot(I, J)
+        }
+    }
+}
+basic.pause(200)
+for (let I = 0; I <= 4; I++) {
+    for (let J = 0; J <= 4; J++) {
+        if (I + J == 4) {
+            led.plot(I, J)
+        }
+    }
+}
+basic.pause(200)
+for (let I = 0; I <= 4; I++) {
+    for (let J = 0; J <= 4; J++) {
+        if (I + J == 3) {
+            led.plot(I, J)
+        }
+    }
+}
+basic.pause(200)
+for (let I = 0; I <= 4; I++) {
+    for (let J = 0; J <= 4; J++) {
+        if (I + J == 2) {
+            led.plot(I, J)
+        }
+    }
+}
+basic.pause(200)
+for (let I = 0; I <= 4; I++) {
+    for (let J = 0; J <= 4; J++) {
+        if (I + J == 1) {
+            led.plot(I, J)
+        }
+    }
+}
+basic.pause(200)
+for (let I = 0; I <= 4; I++) {
+    for (let J = 0; J <= 4; J++) {
+        if (I + J == 0) {
+            led.plot(I, J)
+        }
+    }
+}
